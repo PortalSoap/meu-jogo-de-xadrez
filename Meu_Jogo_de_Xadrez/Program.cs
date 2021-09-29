90c94f6b8269736fee84559e39dd58d7394d730b
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2021-09-29 02:27:18 -0300

    Projeto iniciado.

---
+++ Meu_Jogo_de_Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meu_Jogo_de_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
